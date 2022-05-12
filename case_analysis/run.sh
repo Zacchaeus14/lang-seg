3ddd31ac811d32669bdd8a33f19f2d7117c97984
Zacchaeus14 <yw3642@nyu.edu>
2022-05-13 05:10:27 +0800

    add bad case shell src

--- case_analysis/run.sh
+++ case_analysis/run.sh
@@ -5,8 +5,99 @@ python case_mask.py \
 --img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00001179.jpg \
 --pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00001179.png \
 --gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00001179.png
-#
-#python case_mask.py \
-#--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00000003.jpg \
-#--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00000003.png \
-#--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00000003.png
+
+# 2.
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00004207.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00004207.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00004207.png
+
+# 3. VizWiz_val_00000125
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00000125.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00000125.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00000125.png
+
+# 4. VizWiz_val_00001552
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00001552.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00001552.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00001552.png
+
+# 5. VizWiz_val_00001418 What is on this Captcha please?
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00001418.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00001418.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00001418.png
+
+# 6. VizWiz_val_00003505 What is in...
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00003505.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00003505.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00003505.png
+
+# 7. VizWiz_val_00002687
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00002687.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00002687.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00002687.png
+
+# 8. VizWiz_val_00004072
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00004072.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00004072.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00004072.png
+
+# 9. VizWiz_val_00001112
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00001112.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00001112.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00001112.png
+
+# 10. VizWiz_val_00001101
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00001101.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00001101.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00001101.png
+
+# 11. VizWiz_val_00002798
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00002798.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00002798.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00002798.png
+
+# 12. VizWiz_val_00002379
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00002379.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00002379.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00002379.png
+
+# 13. VizWiz_val_00003884
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00003884.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00003884.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00003884.png
+
+# 14. VizWiz_val_00000127
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00000127.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00000127.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00000127.png
+
+# 15. VizWiz_val_00003286
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00003286.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00003286.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00003286.png
+
+# 16. VizWiz_val_00003812
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00003812.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00003812.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00003812.png
+
+# 17. VizWiz_val_00002480
+python case_mask.py \
+--img /Users/yuchenwang/git/datasets/VizWizGrounding2022/val/VizWiz_val_00002480.jpg \
+--pred /Users/yuchenwang/git/lang-seg/result/mask/val/VizWiz_val_00002480.png \
+--gt /Users/yuchenwang/git/datasets/VizWizGrounding2022/binary_masks_png/val/VizWiz_val_00002480.png
