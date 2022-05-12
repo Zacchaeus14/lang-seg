81142e518c2a318fd39115a77592cd94c3c58e83
Zacchaeus14 <yw3642@nyu.edu>
2022-05-13 04:52:44 +0800

    update bad case src

--- case_mask.py
+++ case_mask.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import json
 
 
 def parse_args():
@@ -10,14 +11,27 @@ def parse_args():
     arg('--img', type=str, required=True)
     arg('--gt', type=str, required=True)
     arg('--pred', type=str, required=True)
+    arg('--anno', type=str, default='/Users/yuchenwang/git/datasets/VizWizGrounding2022/val_grounding.json')
     return parser.parse_args()
 
+def get_iou(mask0, mask1):
+    i = mask0 & mask1
+    u = mask0 | mask1
+    i[i == 255] = 1
+    u[u == 255] = 1
+    return np.sum(i) / np.sum(u)
 
 cfg = parse_args()
 name = cfg.img.split('/')[-1].split('.')[0]
+print(name)
 img = cv2.imread(cfg.img)
 gt = cv2.imread(cfg.gt)[:, :, 0]
 pred = cv2.imread(cfg.pred)[:, :, 0]
+iou = get_iou(gt, pred)
+with open(cfg.anno, 'r') as f:
+    anno = json.load(f)
+question = anno[f'{name}.jpg']['question']
+answer = anno[f'{name}.jpg']['most_common_answer']
 
 color = np.array([255, 0, 0], dtype='uint8')
 masked_img = np.where(gt[..., None], color, img)
@@ -40,4 +54,13 @@ axarr[1].imshow(out_gt)
 axarr[2].imshow(out_pred)
 
 f.tight_layout()
+# # f.suptitle('This is a somewhat long figure title', fontsize=16)
+# w, h = f.get_size_inches()*f.dpi # get fig size in pixels
+# w = int(w)
+# h = int(h)
+# print(f"figure size: w = {w}, h = {h}")
+f.text(0.5, 0.19, f"Q: {question}", ha='center', va='center')
+f.text(0.5, 0.145, f"A: {answer}", ha='center', va='center')
+f.text(0.5, 0.10, "IoU: {:.2f}".format(iou), ha='center', va='center', color='g')
+
 f.savefig(f'case_analysis/case-{name}.jpg', dpi=400)
